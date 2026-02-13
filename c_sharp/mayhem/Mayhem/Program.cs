a9a942ab623a588a366a6560825dbbd6f5b18ce1
Ryan Niemi <blah@sunsetd.com>
2026-02-13 13:33:43 -0800

    Suppress DBus TaskCanceledException on shutdown
    The xdg-desktop-portal DBus connection throws TaskCanceledException
    on a threadpool thread when the Avalonia dispatcher shuts down. Catch
    it in AppDomain.UnhandledException and exit cleanly.

--- c_sharp/mayhem/Mayhem/Program.cs
+++ c_sharp/mayhem/Mayhem/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using System.Threading.Tasks;
 
 namespace Mayhem;
 
@@ -9,8 +10,18 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // DBus connection cleanup races with dispatcher shutdown on Linux,
+        // throwing TaskCanceledException on a threadpool thread. Safe to ignore.
+        AppDomain.CurrentDomain.UnhandledException += (_, e) =>
+        {
+            if (e.ExceptionObject is TaskCanceledException)
+                Environment.Exit(0);
+        };
+
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
